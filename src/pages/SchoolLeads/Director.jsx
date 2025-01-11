7c30ccc7780a9a43d62d3aaa3e5fd88e4d563c62
Kamol <londonglasgow84@gmail.com>
2025-01-12 01:36:21 +0500

    salom

--- src/pages/SchoolLeads/Director.jsx
+++ src/pages/SchoolLeads/Director.jsx
@@ -3,13 +3,13 @@ import { Image, Skeleton } from 'antd';
 import directors from '../../fakeDatas/leaders';
 
 const Director = () => {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 1000);
-    }, []);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setLoading(false);
+    //     }, 1000);
+    // }, []);
 
     if (!directors) {
         return <h2 className="text-center text-red-500">Ma'lumot topilmadi!</h2>;
@@ -19,9 +19,10 @@ const Director = () => {
         <div className='max-w-[1380px] mx-auto p-4 mt-1 font-afacad'>
             <div className='flex flex-row items-start lg:items-start gap-6'>
                 {/* Chap tarafdagi rasm */}
-                <div className='w-[30%] flex items-center justify-center'>
+                <div className='w-[100%] md:w-[30%] flex items-center justify-center'>
                     {loading ? (
-                        <Skeleton.Image className='w-[300px] h-[200px]' active />
+                        // <Skeleton.Image className='w-[300px] h-[200px]' active />
+                        <></>
                     ) : (
                         <Image
                             src={directors?.director.image}
@@ -35,10 +36,10 @@ const Director = () => {
 
                 {/* O'ng tarafdagi ma'lumotlar */}
                 <div className='w-full lg:w-1/2'>
-                    <h1 className='text-2xl sm:text-3xl font-bold text-[#2b82fc] text-center lg:text-left'>
+                    <h1 className='text-md text-start sm:text-xl font-bold text-[#2b82fc] lg:text-left'>
                         {directors?.director.title}
                     </h1>
-                    <p className='text-base sm:text-lg mt-4 text-justify'>
+                    <p className='text-base text-start sm:text-lg mt-4'>
                         {directors?.director.description}
                     </p>
                 </div>
